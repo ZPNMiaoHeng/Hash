7c7a8d9aa9d474e1dde34790883a2b71e5aeacea
220031-Miao Heng <1098011272@qq.com>
2022-09-27 15:59:01 +0800

    con端口优化

--- v/src/sha1_con.v
+++ v/src/sha1_con.v
@@ -1,16 +1,17 @@
 module sha1_con(
-  input         clk   ,
-  input         rst_n ,
-  input         valid ,
+  input         clk     ,
+  input         rst_n   ,
+  input         valid   ,
 
-  output reg [7 :0] t,
-  output  ready_t
+  output [7 :0] t       ,
+  output        ready_t
 );
 
   parameter IDLE  = 2'b00;
   parameter ROUND = 2'b01;
-
+  reg [7 :0] t_tem;
   reg [1 :0] s_cur, s_next;
+
   //-------------------------- First state machine -------------------------------
   always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
@@ -22,12 +23,6 @@ module sha1_con(
   end
 
 //-------------------------- Second state machine -------------------------------
-/*
-  wire [1 :0] s_n;
-  assign s_n = (s_cur == IDLE)? (valid? ROUND: IDLE): ((t < 8'h64)? ROUND: IDLE);
-  s_next = s_n;
-*/
-
   always @(*) begin
     case (s_cur)
       IDLE:begin
@@ -44,7 +39,6 @@ module sha1_con(
         else begin
           s_next = IDLE;
         end
-
       end
       default: s_next = IDLE;
     endcase
@@ -53,11 +47,15 @@ module sha1_con(
 //-------------------------- Third state machine -------------------------------
   always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
-      t <= 7'b0;
+      t_tem <= 7'b0;
     else if(s_cur == ROUND) begin
-      t <= t + 1'b1;
+      t_tem <= t_tem + 1'b1;
     end
-    else t <= 7'b0;
+    else t_tem <= 7'b0;
   end
+  
+//-------------------------------- Result --------------------------------------
   assign ready_t = ((s_cur == ROUND) && (t == 8'h63)) ;
+  assign t = t_tem;
+  
 endmodule
