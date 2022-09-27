9f1b955bb6ebcb2b5a313a5fb4bdefd5a044282b
220031-Miao Heng <1098011272@qq.com>
2022-09-27 17:06:16 +0800

    增加完成状态DONE，修改循环次数bug

--- v/src/sha1_con.v
+++ v/src/sha1_con.v
@@ -9,6 +9,8 @@ module sha1_con(
 
   parameter IDLE  = 2'b00;
   parameter ROUND = 2'b01;
+  parameter DONE  = 2'b10;
+
   reg [7 :0] t_tem;
   reg [1 :0] s_cur, s_next;
 
@@ -33,12 +35,19 @@ module sha1_con(
       end
 
       ROUND: begin
-        if(t < 8'h64) begin
+        if(t < 8'h4e) begin
           s_next = ROUND;
+        end
+        else if (t == 8'h4e) begin
+          s_next = DONE;
         end
         else begin
           s_next = IDLE;
         end
+      end
+
+      DONE: begin
+        s_next = IDLE;
       end
       default: s_next = IDLE;
     endcase
@@ -47,15 +56,20 @@ module sha1_con(
 //-------------------------- Third state machine -------------------------------
   always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
-      t_tem <= 7'b0;
+      t_tem <= 8'b0;
     else if(s_cur == ROUND) begin
       t_tem <= t_tem + 1'b1;
     end
-    else t_tem <= 7'b0;
+    else if(s_cur == DONE) begin
+      t_tem <= t_tem;
+    end
+    else begin
+      t_tem <= 8'b0;
+    end
   end
   
 //-------------------------------- Result --------------------------------------
-  assign ready_t = ((s_cur == ROUND) && (t == 8'h63)) ;
+  assign ready_t = ((s_cur == DONE) && (t == 8'h4f)) ;
   assign t = t_tem;
-  
+
 endmodule
