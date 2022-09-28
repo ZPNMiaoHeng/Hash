85ca8d5c5107d706079c80766398c2f46d0cb8e6
220031-Miao Heng <1098011272@qq.com>
2022-09-28 17:07:42 +0800

    sha1_w.v 未测试

--- v/src/sha1_w.v
+++ v/src/sha1_w.v
@@ -11,22 +11,37 @@
 >> V180121      : World.
 ************************************************************/
 
-module sha1(
-  input clk,
-  input rst_n,
-  input valid,
-  input [511 :0] d_in,
+module sha1
+  #(parameter N = 32)(
+    input          clk   ,
+    input          rst_n ,
+    input          valid ,
+    input [7   :0] t     ,
+    input [511 :0] din   ,
 
-  output reg [159 :0] d_out
+    output [N-1 :0] w
 );
+  reg [512: 0] din_temp;
+  wire [512: 0] din_temp_shift ;
+//-------------------------- 512bits 内存空间 --------------------------------------
+  always @(posedge clk or negedge rst_n ) begin
+    if(!rst_n) begin
+      din_temp <= 512'h0 ;
+    end else if(t == 8'd0) begin
+      din_temp <= din    ;
+    end else if((t > 8'd0) && (t <= 8'd79)) begin
+      din_temp <= din_temp_shift ;
+    end
+  end
 
-  parameter H0 = 32'h6745_2301;  // initialized parameter A,B,C,D,E
-  parameter H1 = 32'hEFCD_AB89;
-  parameter H2 = 32'h98BA_DCFE;
-  parameter H3 = 32'h1032_5476;
-  parameter H4 = 32'hC3D2_E1F0;
+//-------------------------- 生成新的 w --------------------------------------
+  wire [N-1 :0] w_gen_temp = din_temp[511: 480] ^ din_temp[447: 416] ^ din_temp[255: 224] ^ din_temp[95: 64]; // generate new w
+  wire [N-1 :0] w_gen = {w_gen_temp[30: 0], w_gen_temp[31]}
 
-  reg [31 :0] w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15; // d_in = 16 * 32
+  assign din_temp_shift = ((t >= 8'd0) && (t <= 8'd15)) ? {din_temp[479: 0], din_temp[511: 480]} :
+                            (t >= 8'd16) && (t <= 8'd79) ? {din_temp[479: 0], w_gen} : 
+                              512'h0;
 
+  assign w= ((t >= 8'd0) && (t <= 8'd15)) ? din_temp[512: 480] : din_temp[31:0];
 
 endmodule
