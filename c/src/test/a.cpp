7bd75aef38f47e46266b5eed7e2bda7d8b1d1128
220031-Miao Heng <1098011272@qq.com>
2022-09-30 15:26:49 +0800

    学习 C++，并运行示例

---
+++ c/src/test/a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main(){
+ 
+    int scores[3][3] =
+    {
+        {100,100,100},
+        {90,50,100},
+        {60,70,80},
+    };
+ 
+    string names[3] = { "张三","李四","王五" };
+ 
+    for (int i = 0; i < 3; i ++){
+        int sum = 0;
+        for (int j = 0; j < 3; j ++){
+            sum += scores[i][j];
+        }
+        cout << names[i] << "同学总成绩为： " << sum << endl;
+    }
+ 
+    return 0;
+}
